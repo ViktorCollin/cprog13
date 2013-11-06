ea4f803b661d5e6cf5a3b1011a9955b8fc10dfdd
Christian Wemstad <wemstad@kth.se>
2013-11-06 11:12:41 +0100

    Added some new func and test, not really working

--- lab2/Gregorian.h
+++ lab2/Gregorian.h
@@ -21,28 +21,21 @@ namespace lab2{
       Gregorian(const Date&);
       
       virtual int year() const; 
-
       virtual int month() const; 
-
       virtual int day() const; 
-
       virtual int week_day() const; 
-
       virtual int days_per_week() const; 
-
       virtual int days_this_month() const; 
-
       virtual int months_per_year() const;
-
       virtual int days_month(int,int) const;
-
       virtual std::string week_day_name() const; 
-
       virtual std::string month_name() const;
 
       virtual Gregorian& add_year(int n = 1);
       virtual Gregorian& add_month(int n = 1);
       virtual Gregorian& add_day(int n = 1);
+
+      virtual void set_date(int,int,int);
 
       void calcYMD() const; 
       unsigned long long YMDtoNumeric(int,int,int) const; 
