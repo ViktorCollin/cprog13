3e1118b904b9852071c1cd7fc3e772249a7a3823
Viktor Collin <viktor.collin@gmail.com>
2013-11-06 11:34:20 +0100

    nu är operatorerna med i Date.cpp

--- lab2/Date.cpp
+++ lab2/Date.cpp
@@ -11,9 +11,54 @@ namespace lab2 {
     Date::Date(unsigned long long days): _numeric(days) {};
     Date::Date(const Date& d){
         if(this == &d) return;
-        _numeric = d.getDays();
+        _numeric = d.getNumeric();
     }
-    unsigned long long Date::getDays() const {
+    unsigned long long Date::getNumeric() const {
         return _numeric;
     }
+
+    Date& Date::operator++(){
+        _numeric++;
+        return *this;
+    }
+
+    Date& Date::operator--(){
+        _numeric--;
+        return *this;
+    }
+    
+    Date& Date::operator+=(const Date& d){
+        _numeric += d.getNumeric();
+        return *this;
+    }
+    
+    Date& Date::operator-=(const Date& d){
+        _numeric -= d.getNumeric();
+        return *this;
+    }
+
+    inline bool Date::operator==(const Date& d) const{
+        return _numeric == d.getNumeric();
+    }
+
+    inline bool Date::operator!=(const Date& d) const{
+        return _numeric != d.getNumeric();
+    }
+
+    inline bool Date::operator<(const Date& d) const{
+        return _numeric < d.getNumeric();
+    }
+
+    inline bool Date::operator<=(const Date& d) const{
+        return _numeric <= d.getNumeric();
+    }
+
+    inline bool Date::operator>(const Date& d) const{
+        return _numeric > d.getNumeric();
+    }
+
+    inline bool Date::operator>=(const Date& d) const{
+        return _numeric >= d.getNumeric();
+    }
+
 }
