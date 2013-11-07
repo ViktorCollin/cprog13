a455a82bd4cd621bfa1e9afe6686aace760d936b
Christian Wemstad <wemstad@kth.se>
2013-11-07 10:25:04 +0100

    These function needs to be in Date.h

--- lab2/Date.h
+++ lab2/Date.h
@@ -18,15 +18,15 @@ namespace lab2 {
         Date(unsigned long long);
         Date(const Date&);
 
-        //virtual int year() const = 0;
-        //virtual int month() const = 0;
-        //virtual int day() const = 0;
-        //virtual int week_day() const = 0;
-        //virtual int days_per_week() const = 0;
-        //virtual int days_this_month() const = 0;
-        //virtual int months_per_year() const = 0;
-        //virtual std::string week_day_name() const = 0;
-        //virtual std::string month_name() const = 0;
+        virtual int year() const = 0;
+        virtual int month() const = 0;
+        virtual int day() const = 0;
+        virtual int week_day() const = 0;
+        virtual int days_per_week() const = 0;
+        virtual int days_this_month() const = 0;
+        virtual int months_per_year() const = 0;
+        virtual std::string week_day_name() const = 0;
+        virtual std::string month_name() const = 0;
 
         //Operators
         Date& operator=(const Date&);
@@ -41,9 +41,9 @@ namespace lab2 {
         Date& operator-(const Date&);
         long long operator-(const Date&) const;
 
-        //virtual Date& add_year(int n = 1) = 0;
-        //virtual Date& add_month(int n = 1) = 0;
-        //virtual Date& add_day(int n = 1) = 0;
+        virtual Date& add_year(int n = 1) = 0;
+        virtual Date& add_month(int n = 1) = 0;
+        virtual Date& add_day(int n = 1) = 0;
 
         inline bool operator==(const Date&) const; 
         inline bool operator!=(const Date&) const; 
