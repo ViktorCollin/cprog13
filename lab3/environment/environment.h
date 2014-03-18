73b73fe997342d862ae25c2f478de13bc6abd149
wemstad <wemstad@kth.se>
2014-03-18 10:29:31 +0100

    Decl. from assign.

---
+++ lab3/environment/environment.h
@@ -0,0 +1,20 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+namespace the_lion_king_saga {
+
+class Environment {
+	protected:
+
+	public:
+		Environment();
+
+		virtual std::vector<std::String> derections() const;
+		virtual Environment neighbor(std::String);
+		virtual std::String description() const;
+		virtual void enter(Character);
+		virtual void leave(Character);
+
+}
+
+#endif
