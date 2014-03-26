a60b9e19ff3453b7e4d8e62ffe27d40cbcf1459e
Christian Wemstad <wemstad@kth.se>
2014-03-26 15:04:29 +0100

    Removed unused text and files

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,12 +1,9 @@
-
-
 #include <iostream>
 
 using namespace std;
 
 int main(){
     
-	std::cout << "Hej jag heter Christian!\n Detta är berättelsen om hur jag dödade Mufasa!" << std::endl;
     return 0;
     
 }
