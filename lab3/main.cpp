1c1e977c8e244216adc7a71aaca40b5c18065a80
Christian Wemstad <wemstad@kth.se>
2014-04-10 13:20:08 +0200

    Can now handle multiple words as input

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+
+#include "character/user.h"
 
 using namespace std;
 
@@ -10,21 +16,37 @@ void loadState(){
 
 }
 
-void run(){
-	int action;
-	while(1){
-		cin >> action;
-		cout << action << endl;
+void storeState(){
 
-		if(action == 0) break;
+}
+
+void run(the_lion_king_saga::User &user){
+	char c_input[256];
+	string s_input;
+	while(1){
+		//user.list_actions();
+		std::cin.getline (c_input,256);
+		s_input = string(c_input);
+
+		if(s_input == string("exit")) break;
 		else{
-			//handle actions here;
+			//Split
+			istringstream buf(s_input);
+			istream_iterator<string> beg(buf), end;
+			vector<string> actions(beg, end);
+
+			//DO cool stuff....
+			if(actions.size() > 1)
+				cout << actions[1] << endl;
+
 		}
 	}
+	storeState();
 }
 
 int main(){
+	the_lion_king_saga::User* simba = new the_lion_king_saga::User();
 	loadMap();
-	run();
+	run(*simba);
 	return 0;
 }
