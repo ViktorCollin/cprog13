7c551096c953fd3874a28a1c38a88bc440c9fec7
Christian Wemstad <wemstad@kth.se>
2014-04-10 11:37:47 +0200

    Basic I/O implementation

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -2,17 +2,29 @@
 
 using namespace std;
 
-int main(){
-    
-    return 0;
-    
-}
-
 void loadMap(){
-    
+
 }
 
 void loadState(){
-    
+
 }
 
+void run(){
+	int action;
+	while(1){
+		cin >> action;
+		cout << action << endl;
+
+		if(action == 0) break;
+		else{
+			//handle actions here;
+		}
+	}
+}
+
+int main(){
+	loadMap();
+	run();
+	return 0;
+}
