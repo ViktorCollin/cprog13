48410b502e97d0cf9338f31f1c4fe05503890ed7
Christian Wemstad <wemstad@kth.se>
2014-04-10 14:35:29 +0200

    Better input control

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,10 +3,23 @@
 #include <string>
 #include <sstream>
 #include <iterator>
+#include <map>
 
 #include "character/user.h"
 
 using namespace std;
+
+enum actions{
+	Go = 1,
+	Look = 2,
+	Exit = 3
+};
+
+static map<string, actions> mapActions;
+
+
+
+static void init();
 
 void loadMap(){
 
@@ -23,30 +36,42 @@ void storeState(){
 void run(the_lion_king_saga::User &user){
 	char c_input[256];
 	string s_input;
-	while(1){
+	int running = 1;
+	while(running){
 		//user.list_actions();
 		std::cin.getline (c_input,256);
 		s_input = string(c_input);
-
-		if(s_input == string("exit")) break;
-		else{
-			//Split
-			istringstream buf(s_input);
-			istream_iterator<string> beg(buf), end;
-			vector<string> actions(beg, end);
-
-			//DO cool stuff....
-			if(actions.size() > 1)
-				cout << actions[1] << endl;
-
+		//Split
+		istringstream buf(s_input);
+		istream_iterator<string> beg(buf), end;
+		vector<string> reply(beg, end);
+		switch (mapActions[reply[0]]) {
+			case Go : //go
+				if(reply.size() > 1)
+					user.go(reply[1]);
+				else
+					cout << "Need another parameter (Try North :P)" << endl;
+				break;
+			case Exit :
+				running = 0;
+				break;
+			default:
+				cout << "Unknown command" << endl;
 		}
 	}
 	storeState();
 }
 
 int main(){
+	init();
 	the_lion_king_saga::User* simba = new the_lion_king_saga::User();
 	loadMap();
 	run(*simba);
 	return 0;
+}
+
+void init() {
+	mapActions["Go"] = Go;
+	mapActions["Look"] = Look;
+	mapActions["Exit"] = Exit;
 }
