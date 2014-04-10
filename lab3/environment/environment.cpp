cc4f72b7aec56ef89b15d3908dd9ad75d5308128
Viktor Collin <viktor.collin@gmail.com>
2014-04-10 15:55:43 +0200

    shit

--- lab3/environment/environment.cpp
+++ lab3/environment/environment.cpp
@@ -9,4 +9,11 @@ namespace the_lion_king_saga {
 	void Environment::add(Item& item) {
 		std::cout << "Not implemented" << std::endl;
 	}
+	
+	std::string Environment::description() const{
+        return _description;
+	}
+	std::string Environment::shortDescription() const{
+	    return _shortDescription;
+	}
 }
