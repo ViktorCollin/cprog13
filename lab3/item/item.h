6be943f00d0ca91a41c6a0dcec0394ac78919045
Christian Wemstad <wemstad@kth.se>
2014-05-16 15:42:21 +0200

    Formatting

--- lab3/item/item.h
+++ lab3/item/item.h
@@ -5,21 +5,21 @@
 
 namespace the_lion_king_saga {
 
-	class Item {
-		protected:
-			std::string _name;
-			std::string _description;
+    class Item {
+        protected:
+            std::string _name;
+            std::string _description;
             bool _usable;
-			int _weight;
-		public:
-			Item(std::string, std::string, int, bool);
-			Item(std::string, std::string, int);
-			std::string name() const;
-			std::string description() const;
-			int weight() const;
+            int _weight;
+        public:
+            Item(std::string, std::string, int, bool);
+            Item(std::string, std::string, int);
+            std::string name() const;
+            std::string description() const;
+            int weight() const;
             bool isUsable() const;
             virtual void use();
 
-	};
+    };
 }
 #endif
