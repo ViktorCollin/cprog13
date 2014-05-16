72b234ecb63a0efb03e35f10c711c9dedb63624e
Christian Wemstad <wemstad@kth.se>
2014-05-16 15:37:20 +0200

    Tabs to space

--- lab3/item/item.cpp
+++ lab3/item/item.cpp
@@ -4,19 +4,30 @@
 
 namespace the_lion_king_saga {
 
-	Item::Item(std::string name, std::string description, int weight):
-		_name(name), _description(description), _weight(weight) {}
+    Item::Item(std::string name, std::string description, int weight, bool usable):
+        _name(name), _description(description), _weight(weight), _usable(usable) {}
 
-	std::string Item::name() const {
-		return _name;
-	}
+    Item::Item(std::string name, std::string description, int weight):
+        _name(name), _description(description), _weight(weight), _usable(false) {}
 
-	std::string Item::description() const {
-		return _description;
-	}
 
-	int Item::weight() const {
-		return _weight;
-	}
+    std::string Item::name() const {
+        return _name;
+    }
 
+    std::string Item::description() const {
+        return _description;
+    }
+
+    int Item::weight() const {
+        return _weight;
+    }
+
+    bool Item::isUsable() const {
+        return _usable;
+    }   
+
+    void Item::use() {
+        _usable = false;
+    }
 }
--- lab3/item/item.h
+++ lab3/item/item.h
@@ -9,12 +9,16 @@ namespace the_lion_king_saga {
 		protected:
 			std::string _name;
 			std::string _description;
+            bool _usable;
 			int _weight;
 		public:
+			Item(std::string, std::string, int, bool);
 			Item(std::string, std::string, int);
 			std::string name() const;
 			std::string description() const;
 			int weight() const;
+            bool isUsable() const;
+            virtual void use();
 
 	};
 }
